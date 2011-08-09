c89a5bdce067632ed629ef46f0ab17ff062b6c39
Richard Bronosky <richard.bronosky@cmgdigital.com>
2011-08-09 02:21:24 -0400

    Make drag work for relatively positioned elements.

--- swipe/xui.drag.js
+++ swipe/xui.drag.js
@@ -1,70 +1,81 @@
 
 xui.extend({
-    drag : function(callback, options) {
+	drag : function(callback, options) {
 
-        var defaults = {
-            preventDefaultEvents: true,
-            dragCapture: true,
-        };
+		var defaults = {
+			preventDefaultEvents: true,
+			dragCapture: true,
+		};
 
-        for ( var key in defaults ){
-            if(typeof options[key] == "undefined")
+		for ( var key in defaults ){
+			if(typeof options[key] == "undefined")
 				options[key] = defaults[key];
-        }
+		}
 
-        return this.each(function() {
+		return this.each(function() {
 
-            var originalCoord = {
-                x: 0,
-                y: 0
-            };
-            var finalCoord = {
-                x: 0,
-                y: 0,
-                move: false
-            };
-
-
-            function touchStart(e) {
-                originalCoord.x = e.targetTouches[0].clientX;
-                originalCoord.y = e.targetTouches[0].clientY;
-                finalCoord = {x:0, y:0, move:false};
-            }
+			var originalCoord = {
+				x: 0,
+				y: 0
+			};
+			var finalCoord = {
+				x: 0,
+				y: 0,
+				move: false
+			};
 
 
-            function touchMove(e) {
-                if (options.preventDefaultEvents)
-                    e.preventDefault();
+			function touchStart(e) {
+				originalCoord.x = e.targetTouches[0].clientX;
+				originalCoord.y = e.targetTouches[0].clientY;
+				finalCoord = {x:0, y:0, move:0, prior:{}, delta:{}};
+			}
 
-                x = e.targetTouches[0].clientX;
-                y = e.targetTouches[0].clientY;
-				priorCoord = finalCoord
+
+			function touchMove(e) {
+				if (options.preventDefaultEvents)
+					e.preventDefault();
+
+				var t = e.target;
+				finalCoord.prior = {x:finalCoord.x, y:finalCoord.y}
+				finalCoord.x = e.targetTouches[0].clientX;
+				finalCoord.y = e.targetTouches[0].clientY;
+				finalCoord.delta.x = finalCoord.x-finalCoord.prior.x;
+				finalCoord.delta.y = finalCoord.y-finalCoord.prior.y;
 				if (finalCoord.move){
-					e.target.style['left'] = Number(e.target.offsetLeft+x-priorCoord.x)+'px'
-					e.target.style['top'] = Number(e.target.offsetTop+y-priorCoord.y)+'px'
+					t.style.left = parseInt(t.style.left)+finalCoord.delta.x+'px';
+					t.style.top = parseInt(t.style.top)+finalCoord.delta.y+'px';
+				} else {
+					// first move, reconcile the potential difference between absolute and relative postioning
+					finalCoord.delta={x:t.offsetLeft, y:t.offsetTop};
+					t.style.left = t.offsetLeft+finalCoord.delta.x+'px';
+					t.style.left = parseInt(t.style.left)-t.offsetLeft+finalCoord.delta.x+'px';
+					t.style.top = t.offsetTop+finalCoord.delta.y+'px';
+					t.style.top = parseInt(t.style.top)-t.offsetTop+finalCoord.delta.y+'px';
+
 				}
-				finalCoord = {x:x, y:y, move:true};
-            }
+				finalCoord.move = finalCoord.move+1;
+			}
 
 
-            function touchEnd(e) {
-                if (options.preventDefaultEvents)
-                    e.preventDefault();
+			function touchEnd(e) {
+				if (options.preventDefaultEvents)
+					e.preventDefault();
 
-                var coords = {};
+				var coords = {};
 
-                if (options.dragCapture) {
-                    coords.deltaX = finalCoord.x - originalCoord.x;
-                    coords.deltaY = originalCoord.y- finalCoord.y;
+				if (options.dragCapture) {
+					coords.deltaX = finalCoord.x - originalCoord.x;
+					coords.deltaY = originalCoord.y- finalCoord.y;
 					return callback(e, coords);
-                }
-            }
+				}
+			}
 
-            this.addEventListener("touchstart", touchStart, false);
-            this.addEventListener("touchend", touchEnd, false);
+			this.addEventListener("touchstart", touchStart, false);
+			this.addEventListener("touchend", touchEnd, false);
 
-            if (options.dragCapture)
-                this.addEventListener("touchmove", touchMove, false);
-        });
-    }
+			if (options.dragCapture)
+				this.addEventListener("touchmove", touchMove, false);
+		});
+	}
 });
